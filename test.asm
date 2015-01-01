234f1563fabdd33ea803aa77d07012b1c5056333
AInoob <ainoob@qq.com>
2014-12-31 23:46:32 -0600

    Add first old asm file and setting for bochs

---
+++ test.asm
@@ -0,0 +1,45 @@
+[BITS 16]
+[ORG 0x7c00]
+
+jmp     Main
+
+PrintTill0:
+lodsb
+cmp     al,0
+je      Return
+mov     ah,0eh
+int     10h
+jmp     PrintTill0
+
+PrintWithLength:
+mov     ah,13h
+int     10h
+jmp     Return
+
+Return:
+ret
+
+Main:
+mov     si,msg1
+call    PrintTill0
+
+cli
+push    SEGment msg2
+pop     es
+mov     bp,msg2
+sti
+loop1:
+mov     cx,9
+call    PrintWithLength
+inc     bl
+jmp     loop1
+
+cli
+hlt
+
+
+msg1:    db  "Hello, AInoob~~~",0Dh,0Ah,"The future is in your hand, so, what do you want to do?",0Dh,0Ah,0h
+msg2:    db  "AInoob~~~"
+
+times 510-($-$$) db 0
+dw 0xAA55
